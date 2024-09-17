d72a9231ac2dea9eae9a7ca3ef8593e93b384163
88saiba <kuroganesaiba@gmail.com>
2024-09-17 12:45:10 +0700

    additem

---
+++ Think_python/app.py
@@ -0,0 +1,8 @@
+class HelloWorld:
+    # This method receives a message and prints it to the terminal
+    @staticmethod
+    def print_message(message):
+        print(message)  # Output the message
+
+# The data is passed to the print_message method
+HelloWorld.print_message("Engine Started")
