eb3cf1661665c4ad607082622c48350682ceb0a8
88saiba <yatsugisaiba@gmail.com>
2024-10-10 19:57:43 +0700

    update main.lua

--- SelfTaught/lua3xp/moving_clicker_game/main.lua
+++ SelfTaught/lua3xp/moving_clicker_game/main.lua
@@ -3,7 +3,7 @@ local score = 0
 local buttonX, buttonY
 local buttonWidth, buttonHeight = 200, 100
 
--- Fungsi untuk mengatur posisi tombol secara acak
+-- Function to randomly set button positions
 function moveButton()
     buttonX = love.math.random(0, love.graphics.getWidth() - buttonWidth)
     buttonY = love.math.random(0, love.graphics.getHeight() - buttonHeight)
@@ -12,28 +12,28 @@ end
 function love.load()
     love.window.setTitle("Moving Clicker Game")
     love.window.setMode(800, 600)
-    moveButton() -- Pindahkan tombol saat mulai
+    moveButton() -- Move button on start
 end
 
 function love.draw()
-    -- Gambar latar belakang
-    love.graphics.clear(1, 1, 1) -- Putih
-    love.graphics.setColor(0, 0, 0) -- Hitam
+    -- Background image
+    love.graphics.clear(1, 1, 1) -- White
+    love.graphics.setColor(0, 0, 0) -- Black
     love.graphics.print("Score: " .. score, 10, 10)
 
-    -- Gambar tombol
-    love.graphics.setColor(0, 0.5, 1) -- Biru
+    -- Button image
+    love.graphics.setColor(0, 0.5, 1) -- Blue
     love.graphics.rectangle("fill", buttonX, buttonY, buttonWidth, buttonHeight)
-    love.graphics.setColor(1, 1, 1) -- Putih
+    love.graphics.setColor(1, 1, 1) -- White
     love.graphics.printf("Click Me!", buttonX, buttonY + 35, buttonWidth, "center")
 end
 
 function love.mousepressed(x, y, button, istouch, presses)
-    if button == 1 then -- Klik kiri
+    if button == 1 then -- Left click
         if x >= buttonX and x <= buttonX + buttonWidth and
            y >= buttonY and y <= buttonY + buttonHeight then
-            score = score + 1 -- Tambah skor
-            moveButton() -- Pindahkan tombol secara acak
+            score = score + 1 -- Add up the score
+            moveButton() -- Move buttons randomly
         end
     end
 end
