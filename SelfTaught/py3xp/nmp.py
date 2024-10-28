fc915dae7ce86057bcf445c8a54d071026b2c875
88saiba <yatsugisaiba@gmail.com>
2024-10-28 12:46:31 +0700

    upd nmp.py

--- SelfTaught/py3xp/nmp.py
+++ SelfTaught/py3xp/nmp.py
@@ -8,30 +8,36 @@ def start_services():
     """Start Nginx and MySQL."""
     print("Starting The Nginx with MySQL...")
     run_command("sudo systemctl enable nginx")
-    run_command("sudo systemctl start nginx")
-    run_command("sudo systemctl status nginx")
-    
     run_command("sudo systemctl enable mysql.service")
+    
+    run_command("sudo systemctl start nginx")
     run_command("sudo systemctl start mysql.service")
+    print("---------------------------------")
+    run_command("sudo systemctl status nginx")
+    print("---------------------------------")
     run_command("sudo systemctl status mysql.service")
+
 
 def stop_services():
     """Stop Nginx and MySQL."""
     print("Stopping The Nginx and MySQL...")
+    run_command("sudo systemctl stop nginx")
     run_command("sudo systemctl stop mysql.service")
+    
+    run_command("sudo systemctl disable nginx")
     run_command("sudo systemctl disable mysql.service")
+    print("---------------------------------")
+    run_command("sudo systemctl status nginx")
+    print("---------------------------------")
     run_command("sudo systemctl status mysql.service")
     
-    run_command("sudo systemctl stop nginx")
-    run_command("sudo systemctl disable nginx")
-    run_command("sudo systemctl status nginx")
 
 def main():
     """Main menu of the program."""
     while True:
         print("\nMenu:")
         print("1. Turn Engine On")
-        print("2. Turn Engine On")
+        print("2. Turn Engine Off")
         print("3. Bail Out")
         
         choice = input("Choose an option (1/2/3): ")
