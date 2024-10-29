cad77a86531217d525307826247cec737abb2bdc
88saiba <yatsugisaiba@gmail.com>
2024-10-29 10:51:04 +0700

    upd nmp.py

--- SelfTaught/py3xp/nmp.py
+++ SelfTaught/py3xp/nmp.py
@@ -8,11 +8,12 @@ def run_command(command):
 def start_services():
     """Start Nginx and MySQL."""
     print("Starting The Nginx with MySQL...")
-    run_command("sudo systemctl enable nginx")
-    run_command("sudo systemctl enable mysql.service")
+    print("--------------------------------->")
+    run_command("sudo systemctl enable nginx mysql.service")
+    # run_command("sudo systemctl enable mysql.service")
     
-    run_command("sudo systemctl start nginx")
-    run_command("sudo systemctl start mysql.service")
+    run_command("sudo systemctl start nginx mysql.service")
+    # run_command("sudo systemctl start mysql.service")
     print("---------------------------------")
     run_command("sudo systemctl status nginx")
     print("---------------------------------")
@@ -22,11 +23,12 @@ def start_services():
 def stop_services():
     """Stop Nginx and MySQL."""
     print("Stopping The Nginx and MySQL...")
-    run_command("sudo systemctl stop nginx")
-    run_command("sudo systemctl stop mysql.service")
+    print("--------------------------------->")
+    run_command("sudo systemctl stop nginx mysql.service")
+    # run_command("sudo systemctl stop mysql.service")
     
-    run_command("sudo systemctl disable nginx")
-    run_command("sudo systemctl disable mysql.service")
+    run_command("sudo systemctl disable nginx mysql.service")
+    # run_command("sudo systemctl disable mysql.service")
     print("---------------------------------")
     run_command("sudo systemctl status nginx")
     print("---------------------------------")
@@ -42,17 +44,26 @@ def check_services():
     print("---------------------------------")
     run_command("sudo systemctl status mysql.service")
     
+def monitor_nginx():
+    """Monitoring Nginx using multitail """
+    print("Monitoring Nginx...")
+    time.sleep(1)
+    run_command("sudo multitail /var/log/nginx/access.log /var/log/nginx/error.log")
 
 def main():
     """Main menu of the program."""
     while True:
-        print("\nMenu:")
-        print("1. Turn Engine On")
-        print("2. Turn Engine Off")
+        print("\n+===========+")
+        print("  NMP-Menu")
+        print("+===========+")
+        print("1. Turn On")
+        print("2. Turn Off")
         print("3. Check Engine")
-        print("4. Bail Out")
+        print("4. View Logs")
+        print("5. Bail Out")
+        print("+===========+")
         
-        choice = input("Choose an option (1/2/3/4): ")
+        choice = input("Choose an option (1/2/3/4/5): ")
         
         if choice == '1':
             start_services()
@@ -61,6 +72,8 @@ def main():
         elif choice == '3':
             check_services()
         elif choice == '4':
+            monitor_nginx()
+        elif choice == '5':
             print("Exiting the program.")
             os.system('clear')
             break
