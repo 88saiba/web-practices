b966aa63800033ac9febaab24c6e3974dbc9965a
88saiba <yatsugisaiba@gmail.com>
2024-10-29 13:11:53 +0700

    updemp

--- SelfTaught/py3xp/emp.py
+++ SelfTaught/py3xp/emp.py
@@ -95,8 +95,9 @@ def main():
     while True:
         os.system('clear')
         print("+===========+")
-        print("[Nginx Mysql Php]")
-        print("  EMP-Menu")
+        print("Nginx, Mysql, Php")
+        print("-------------")
+        print("EMP-Menu")
         print("+===========+")
         print("1. Turn On Engine")
         print("2. Turn Off Engine")
